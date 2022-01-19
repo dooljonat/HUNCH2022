dc8161d8881964767e022dafe1eb3f4ed26ec52a
Jonathan Dooley <jonathandool75@gmail.com>
2022-01-19 10:20:23 -0500

    Created code skeleton for views and urls

--- hunchdjango/fermentationlab/urls.py
+++ hunchdjango/fermentationlab/urls.py
@@ -4,4 +4,6 @@ from django.urls import path
 app_name = "fermentationlab"
 urlpatterns = [
     path('', views.index, name='index'),
+    path('charts/temperatures/<int:look_back>', views.get_temperatures, name='temperature_data'),
+    path('charts/co2levels/<int:look_back>', views.get_co2levels, name='co2_level_data')
 ]
--- hunchdjango/fermentationlab/views.py
+++ hunchdjango/fermentationlab/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from .models import Temperature
+
+from .models import Temperature, CO2Level
 
 # Create your views here.
 def index(request):
     return render(request, 'fermentationlab/index.html', {})
+
+
+"""TODO: Make these return temperature and CO2Level
+   data in json format, going as far back from the current date as look_back
+"""
+def get_temperatures(request, look_back):
+    pass
+
+def get_co2levels(request, look_back):
+    pass
