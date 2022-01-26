be22d39a31a1ccc3c648d35115e23fc5ee8ef252
Jonathan Dooley <jonathandool75@gmail.com>
2022-01-26 11:14:31 -0500

    Created funguslab

--- run.bat
+++ run.bat
@@ -1,3 +1,4 @@
 echo FERMENTATIONLAB 
 start /min cmd /c python modularize\fermentationlab\fermentationlab\manage.py runserver 8000
 start /min cmd /c python modularize\crystallab\crystallab\manage.py runserver 8010
+start /min cmd /c python modularize\funguslab\funguslab\manage.py runserver 8020
