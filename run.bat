f47b8f6d2522c3848841557641a394aa934a2e72
Jonathan Dooley <jonathandool75@gmail.com>
2022-01-26 11:16:43 -0500

    Created hub

--- run.bat
+++ run.bat
@@ -1,4 +1,5 @@
 echo FERMENTATIONLAB 
-start /min cmd /c python modularize\fermentationlab\fermentationlab\manage.py runserver 8000
+start /min cmd /c python modularize\hub\hub\manage.py runserver 8000
 start /min cmd /c python modularize\crystallab\crystallab\manage.py runserver 8010
 start /min cmd /c python modularize\funguslab\funguslab\manage.py runserver 8020
+start /min cmd /c python modularize\fermentationlab\fermentationlab\manage.py runserver 8000
