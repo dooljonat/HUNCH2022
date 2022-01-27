26aef6ec5e0935adb91e4637eee5de1713e61d66
Jonathan Dooley <jonathandool75@gmail.com>
2022-01-27 10:12:44 -0500

    Finished porting

--- hunchdjango/fermentationlab/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Temperature, CO2Level
-
-# Register your models here.
-admin.site.register(Temperature)
-admin.site.register(CO2Level)
--- hunchdjango/fermentationlab/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FermentationlabConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'fermentationlab'
