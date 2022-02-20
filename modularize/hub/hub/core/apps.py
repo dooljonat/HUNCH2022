ba9fccf695d4bfa08533dc280906a33c6a69e8e3
Rihay66 <noiralter0660@gmail.com>
2022-02-20 17:36:03 -0500

    Adding in celery to project

--- modularize/hub/hub/core/apps.py
+++ modularize/hub/hub/core/apps.py
@@ -1,5 +1,9 @@
 from django.apps import AppConfig
 
+class CeleryWorker(AppConfig):
+    name = 'djcelery'
+    
+
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
