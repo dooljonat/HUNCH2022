4b1826d3fda778e7f780d9d593f60d369543ff45
Jonathan <jonathandool75@gmail.com>
2022-02-21 20:52:48 -0500

    Got picture taking working through sockets

--- modularize/hub/hub/hub/celery_settings.py
+++ modularize/hub/hub/hub/celery_settings.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, unicode_literals
 
 import os
 import time
+from datetime import datetime
 import socket
 import json
 
@@ -26,7 +27,7 @@ HOST = '172.20.10.7'
 PORT = 65439
 
 from django.conf import settings
-from fermentationlab.models import Temperature, Humidity
+from fermentationlab.models import Temperature, Humidity, PiCameraImage
 
 
 @app.on_after_configure.connect
@@ -87,6 +88,66 @@ def send_data_request():
        print("Failed to retreive data from Raspberry Pi")
    sock.close()
    time.sleep(1)
+
+@app.task
+def send_picture_request(username):
+    """Sends a request for data from the Raspberry Pi, receives it, and adds it to the DB"""
+
+    # instantiate a socket object
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    print('socket instantiated')
+
+    # connect the socket
+    connectionSuccessful = False
+    while not connectionSuccessful:
+        try:
+            sock.connect((HOST, PORT))    # Note: if execution gets here before the server starts up, this line will cause an error, hence the try-except
+            print('socket connected')
+            connectionSuccessful = True
+        except:
+            pass
+
+        socks = [sock]
+
+        message = "take_picture"
+        sock.sendall(message.encode())
+
+        # Get the current date
+        created_on = datetime.now()
+        created_on_string = str(created_on)
+        created_on_string = created_on_string.replace(" ", "_")
+        created_on_string = created_on_string.replace(":", "__")
+
+        # Create file path for image, if it doesnt exist, create it
+        file_path = f'media/img/{username}/'
+        if not os.path.exists(file_path):
+            os.makedirs(file_path)
+
+        # Open the image file and read it from socket connection
+        file_path += f"{created_on_string}.jpg"
+        print(file_path)
+        
+        file = open(file_path, "wb")
+        image_chunk = sock.recv(4096)
+        while image_chunk:
+            file.write(image_chunk)
+            image_chunk = sock.recv(4096)
+
+        file.close()
+
+        # Remove media from the path
+        file_path = file_path.replace("media/", "")
+        print(file_path)
+
+        # Create new PiCameraImage model from image file
+        picture = PiCameraImage()
+        picture.created_on = created_on
+        picture.upload = file_path
+        picture.save()
+    sock.close()
+    time.sleep(1)
+
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
